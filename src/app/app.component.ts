74a04b82a71e9b25539529aa02a4a29acc10c6b0
m.saharov <maxim.saharov501@gmail.com>
2022-10-19 23:51:27 +0300

    Add ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, VERSION} from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend-remote';
+
+  //title = 'frontend-remote';
+
+  public title = `Angular ${VERSION.major} i18n with ngx-translate`;
+  public customNumberValue = 12345;
+
+  constructor(public translateService: TranslateService) {
+    this.translateService.addLangs(["tr", "en", "se"]);
+  }
+
+  public get translationFormTypeScript(): string {
+    return this.translateService.instant("example5.fromTypeScript");
+  }
+
+  public onChange(selectedLanguage: string): void {
+    this.translateService.use(selectedLanguage);
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+import {AppComponent} from './app.component';
 import {ErrorPageComponent} from './components/error-page/error-page.component';
+
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+   return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
    declarations: [
@@ -17,7 +25,16 @@ import {ErrorPageComponent} from './components/error-page/error-page.component';
       AppRoutingModule,
       NoopAnimationsModule,
       ReactiveFormsModule,
-      FormsModule
+      FormsModule,
+      HttpClientModule,
+      TranslateModule.forRoot({
+         loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+         },
+         defaultLanguage: 'tr'
+      })
    ],
    providers: [],
    bootstrap: [AppComponent]
